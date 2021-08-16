740f2fc7118247613a6dbaf2f95b489c2f8c1c12
alimrankhan <alimrankhan54@gmail.com>
2021-08-17 00:53:13 +0600

    2nd commit

--- testsite/settings.py
+++ testsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'test',
+    'travello',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')], 
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #mycode
 STATICFILES_DIRS= [ os.path.join(BASE_DIR, 'static') ]
-STATIC_ROOT=os.path.join( BASE_DIR, 'assets') 
+STATIC_ROOT=os.path.join( BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
