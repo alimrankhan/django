0639bf14b28f4d5b5a89a393033ff1eab1cfb162
alimrankhan <alimrankhan54@gmail.com>
2021-09-05 18:06:07 +0600

    telusko completed

--- reg/urls.py
+++ reg/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns= [
     path('',views.index, name= 'registration'),
+    path('login/', views.login, name= 'login'),
+    path('logout/', views.logout, name= 'logout')
 ]
--- reg/views.py
+++ reg/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 def index(request):
     if request.method == 'POST' :
         fname= request.POST['fname']
@@ -10,12 +11,38 @@ def index(request):
         email= request.POST['email']
         password1= request.POST['password1']
         password2= request.POST['password2']
+        if password1!=password2:
+            print('password not matched')
+            messages.info(request, "Password not matched")
+            return redirect('/reg')
+        elif(User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+            print('username or email is already registerd')
+            messages.info(request, "username or email is already registerd")
+            return redirect('/reg')
+        else:
+            user= User.objects.create_user(first_name= fname, last_name= lname, username= username, email= email, password= password1)
+            user.save();
+            print('User registerd')
+            messages.info(request, "User registerd successfully")
+        return redirect('/reg')
 
-        user= User.objects.create_user(first_name= fname, last_name= lname, username= username, email= email, password= password1)
-        user.save();
-        print('User registerd')
-        return redirect('/')
-# TODO: check password, username, email validation
     else:
-        return render(request,'reg.html', {'method':request.method})
-# TODO: create login logout
+        return render(request,'reg.html', )
+
+def login(request):
+    if request.method== 'POST':
+        username= request.POST['username']
+        password= request.POST['password']
+        user= auth.authenticate(username= username, password= password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid username or password')
+            return redirect('/reg/login/')
+    else:
+        return render(request, 'login.html')
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
